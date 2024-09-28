1fc1bc2de22b2e1526555f469423d7350819a386
RQM <rqmartins73@gmail.com>
2024-09-28 17:08:58 +0100

    Added List Volume Clones in all Workspaces

--- bluexport.sh
+++ bluexport.sh
@@ -70,8 +70,8 @@ then
 fi
 ####  END: Check if Config File exists  ####
 
-declare -A wsmap
-declare -A allws_array
+declare -a wsmap
+declare -a allws_array
 
 ####  START: Get Cloud Config Data  #####
 resource_grp=$(cat $bluexscrt | grep -w "RESOURCE_GRP" | awk {'print $2'})
