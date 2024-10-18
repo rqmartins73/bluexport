8902c801d14df68afc4772371d0281e0db4673d2
RQM <rqmartins73@gmail.com>
2024-10-18 16:44:04 +0100

    snapdel - Change the way the VSI is checked for existence

--- bluexport.sh
+++ bluexport.sh
@@ -834,14 +834,14 @@ case $1 in
 	fi
 	test=0
 	flagj=1
-	vsi_name=$2
+#	vsi_name=$2
 	vsi=$2
 	vsi_id_bluexscrt
 	snap_name=$3
-	vsi_ws=$(cat $bluexscrt | grep $vsi_name | awk {'print $4'})
+	vsi_ws=$(cat $bluexscrt | grep $vsi | awk {'print $4'})
 	vsi_ws_id=$(cat $bluexscrt | grep -m 1 $vsi_ws | awk {'print $2'})
-	vsi_id=$(cat $bluexscrt | grep $vsi_name | awk {'print $3'})
-	echo "`date +%Y-%m-%d_%H:%M:%S` - === Starting Snapshot Delete $snap_name from VSI $vsi_name !" >> $log_file
+	vsi_id=$(cat $bluexscrt | grep $vsi | awk {'print $3'})
+	echo "`date +%Y-%m-%d_%H:%M:%S` - === Starting Snapshot Delete $snap_name from VSI $vsi !" >> $log_file
 	cloud_login
 #	check_VSI_exists
 	/usr/local/bin/ibmcloud pi ws tg $vsi_ws_id
