92f23417b7a4b197d3274626984c394dfaf83179
RQM <rqmartins73@gmail.com>
2025-05-19 15:41:33 +0100

    Corrected an error in -snapcr when not selecting all Volumes

--- bluexport.sh
+++ bluexport.sh
@@ -40,7 +40,7 @@
 
        #####  START:CODE  #####
 
-Version=3.7.8
+Version=3.7.9
 log_file=$(cat $HOME/bluexport.conf | grep -w "log_file" | awk {'print $2'})
 bluexscrt=$(cat $HOME/bluexport.conf | grep -w "bluexscrt" | awk {'print $2'})
 end_log_file='==== END ========= $timestamp ========='
