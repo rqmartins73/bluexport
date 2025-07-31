4ab1a725cfa22b18b63a5a37af6ab2a2bca6a1d2
RQM <rqmartins73@gmail.com>
2025-07-31 16:54:33 +0100

    Bug fixes, and changes in the way it get the iASP names

--- bluexport.sh
+++ bluexport.sh
@@ -40,7 +40,7 @@
 
        #####  START:CODE  #####
 
-Version=3.8.12
+Version=3.7.7-17-gfb1b1de
 log_file=$(cat $HOME/bluexport.conf | grep -w "log_file" | awk {'print $2'})
 bluexscrt=$(cat $HOME/bluexport.conf | grep -w "bluexscrt" | awk {'print $2'})
 end_log_file='==== END ========= $timestamp ========='
